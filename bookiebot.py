89d0fb96ff0cdbfca6e849b1e9b4387879d4b78b
Michael Neynens <m.neynens@pbsa.info>
2018-06-16 18:45:45 -0700

    Added exception handling

--- bookiebot.py
+++ bookiebot.py
@@ -18,9 +18,12 @@ def odds_round(x):
 		return float('%.2f'%(x))
 
 def cancelUnmatchedBets(ppy):
-	unmatched = json.loads(json.dumps(bookie.getUnmatchedBets("1.2.769"))) # account id of bettor
+	unmatched = json.loads(json.dumps(bookie.getUnmatchedBets("1.2.769"))) # account id of market-maker
 	for bet in unmatched:
-		print(ppy.bet_cancel(bet['id']))
+		try:
+			print(ppy.bet_cancel(bet['id']))
+		except:
+			print('Could not cancel bet.')
 
 def placeBetHelper(runners, mappings, ppy):
 	print("Entered placeBetHelper")
@@ -35,14 +38,17 @@ def placeBetHelper(runners, mappings, ppy):
 				for market in markets: # find the betting market for this team
 					if (market['description'][0][1] == runner['name'] or market['description'][0][1][-4:].upper() == runner['name'][:4]):
 
-						print("Betting on betting market ", market['id'])
-						if (runner['prices'][0]):
-							print(ppy.bet_place(market['id'], Amount(random.uniform(0.02, 0.08), "BTF"), odds_round(runner['prices'][0]['odds']), runner['prices'][0]['side']))
-						if (runner['prices'][1]):
-							print(ppy.bet_place(market['id'], Amount(random.uniform(0.02, 0.08), "BTF"), odds_round(runner['prices'][1]['odds']), runner['prices'][1]['side']))
+						try:
+							print("Betting on betting market ", market['id'])
+							if (runner['prices'][0]):
+								print(ppy.bet_place(market['id'], Amount(random.uniform(0.02, 0.08), "BTF"), odds_round(runner['prices'][0]['odds']), runner['prices'][0]['side']))
+							if (runner['prices'][1]):
+								print(ppy.bet_place(market['id'], Amount(random.uniform(0.02, 0.08), "BTF"), odds_round(runner['prices'][1]['odds']), runner['prices'][1]['side']))
+						except:
+							print('Handling exception... bad odds?')
 
 if __name__ == '__main__':
-	ppy = PeerPlays(nobroadcast=True)
+	ppy = PeerPlays(nobroadcast=False)
 	ppy.wallet.unlock(bookie.pwd)
 	url = "https://api.matchbook.com/edge/rest/events?category-ids=15,400798618290009,410468520880009,617663353250012&exchange-type=back-lay&odds-type=DECIMAL&price-depth=1"
 	with open('mapping.txt', 'r') as mappingsfile:
@@ -69,8 +75,9 @@ if __name__ == '__main__':
 					something_in_play = True
 		if (something_in_play):
 			print("Something in play, sleeping for 20s")
+			something_in_play = False
 			time.sleep(20)
 		else:
-			print("Nothing in play, sleeping for 1 hour")
+			print("Nothing in play, sleeping for 15m")
 			something_in_play = False
-			time.sleep(3600)
+			time.sleep(900)
