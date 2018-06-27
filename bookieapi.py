498fc3bc4b4de24504b3ddf285862adbe530ce51
Michael Neynens <m.neynens@pbsa.info>
2018-06-27 14:57:35 -0700

    Added optional account parameter on placeBets

--- bookieapi.py
+++ bookieapi.py
@@ -13,6 +13,7 @@ ppy.wallet.unlock(bookie.pwd)
 
 @app.route("/placeBets", methods=['POST'])
 def placeBets():
+	account = request.args['account']
 	body = request.get_json()
 	response = []
 	for bet in body['bets']:
@@ -24,7 +25,7 @@ def placeBets():
 		a  = Amount(bet_amount, asset_symbol)
 		#right now, we will place bets successfully one by one until one breaks.
 		#the user will be confused whether any of the bets got placed or not
-		bet_response = ppy.bet_place(betting_market_id, a, odds, back_or_lay)
+		bet_response = ppy.bet_place(betting_market_id, a, odds, back_or_lay, account)
 		response.append(bet_response)
 	return jsonify(response)
 
@@ -40,31 +41,32 @@ def getUnmatchedBets(bettor_id):
 
 @app.route("/bettors/<bettor_id>/history", methods=['GET'])
 def getHistory(bettor_id):
-	return jsonify(bookie.getHistory(bettor_id))
+	limit = int(request.args['limit'])
+	return jsonify(bookie.getHistory(bettor_id,limit))
 
 @app.route("/sports", methods=['GET'])
 def getSports():
 	return jsonify(bookie.getSports())
 
-@app.route("/sports/<sport_id>/eventGroups")
+@app.route("/sports/<sport_id>/eventGroups", methods=['GET'])
 def getEventGroups(sport_id):
 	return jsonify(bookie.getEventGroups(sport_id))
 
-@app.route("/eventGroups/<event_group_id>/events")
+@app.route("/eventGroups/<event_group_id>/events", methods=['GET'])
 def getEvents(event_group_id):
 	return jsonify(bookie.getEvents(event_group_id))
 
-@app.route("/events/<event_id>/bettingMarketGroups")
+@app.route("/events/<event_id>/bettingMarketGroups", methods=['GET'])
 def getBettingMarketGroups(event_id):
 	return jsonify(bookie.getBettingMarketGroups(event_id))
 
-@app.route("/bettingMarketGroups/<bmg_id>/bettingMarkets")
+@app.route("/bettingMarketGroups/<bmg_id>/bettingMarkets", methods=['GET'])
 def getBettingMarkets(bmg_id):
 	return jsonify(bookie.getBettingMarkets(bmg_id))
 
-@app.route("/rules/<rules_id>")
+@app.route("/rules/<rules_id>", methods=['GET'])
 def getRules(rules_id):
 	return jsonify(bookie.getRules(rules_id))
 
 if __name__ == '__main__':
-	app.run(debug=False, host='0.0.0.0')
+	app.run(debug=False, host='0.0.0.0', port=5001)
