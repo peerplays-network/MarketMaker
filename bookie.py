d784255c31697f76002a18847677b73f6b1716b6
Michael Neynens <m.neynens@pbsa.info>
2018-07-18 21:25:14 -0700

    Added bettingMarketByQuery and individual resource endpoints

--- bookie.py
+++ bookie.py
@@ -21,25 +21,40 @@ def getMatchedBets(bettor_id):
 	matched_bets = ppy.rpc.get_matched_bets_for_bettor(bettor_id, api="bookie")
 	return matched_bets
 
+def getSport(sport_id):
+	return Sport(sport_id, peerplays_instance=ppy)
+
 def getSports():
 	sports = Sports(peerplays_instance=ppy)
 	# TODO Add pagination
 	return sports.sports
+
+def getEventGroup(event_group_id):
+	return EventGroup(event_group_id, peerplays_instance=ppy)
 
 def getEventGroups(sport_id):
 	event_groups = Sport(sport_id, peerplays_instance=ppy)
 	# TODO add pagination
 	return event_groups.eventgroups
 
+def getEvent(event_id):
+	return Event(event_id, peerplays_instance=ppy)
+
 def getEvents(event_group_id):
 	events = EventGroup(event_group_id, peerplays_instance=ppy)
 	# TODO add pagination
 	return events.events
 
+def getBettingMarketGroup(bmg_id):
+	return BettingMarketGroup(bmg_id, peerplays_instance=ppy)
+
 def getBettingMarketGroups(event_id):
 	bmgs = Event(event_id, peerplays_instance=ppy)
 	# TODO add pagination
 	return bmgs.bettingmarketgroups
+
+def getBettingMarket(bm_id):
+	return BettingMarket(bm_id, peerplays_instance=ppy)
 
 def getBettingMarkets(bmg_id):
 	betting_markets = BettingMarketGroup(bmg_id, peerplays_instance=ppy)
@@ -47,9 +62,8 @@ def getBettingMarkets(bmg_id):
 	return betting_markets.bettingmarkets
 
 def getRules(rules_id):
-	rules = Rule(rules_id, peerplays_instance=ppy)
 	# TODO add pagination
-	return rules
+	return Rule(rules_id, peerplays_instance=ppy)
 
 def getHistory(bettor_id, limit=100):
 	a = Account(bettor_id, peerplays_instance=ppy, full=True)
--- bookieapi.py
+++ bookieapi.py
@@ -92,10 +92,24 @@ def getMatchedBets(bettor_id):
 	except Exception as e:
 		return make_response(jsonify(error=e.__doc__), 500)
 
+@app.route("/sports/<sport_id>", methods=['GET'])
+def getSport(sport_id):
+	try:
+		return jsonify(bookie.getSport(sport_id))
+	except Exception as e:
+		return make_response(jsonify(error=e.__doc__), 500)
+
 @app.route("/sports", methods=['GET'])
 def getSports():
 	try:
 		return jsonify(bookie.getSports())
+	except Exception as e:
+		return make_response(jsonify(error=e.__doc__), 500)
+
+@app.route("/eventGroups/<event_group_id>", methods=['GET'])
+def getEventGroup(event_group_id):
+	try:
+		return jsonify(bookie.getEventGroup(event_group_id))
 	except Exception as e:
 		return make_response(jsonify(error=e.__doc__), 500)
 
@@ -106,6 +120,13 @@ def getEventGroups(sport_id):
 	except Exception as e:
 		return make_response(jsonify(error=e.__doc__), 500)
 
+@app.route("/events/<event_id>", methods=['GET'])
+def getEvent(event_id):
+	try:
+		return jsonify(bookie.getEvent(event_id))
+	except Exception as e:
+		return make_response(jsonify(error=e.__doc__), 500)
+
 @app.route("/eventGroups/<event_group_id>/events", methods=['GET'])
 def getEvents(event_group_id):
 	try:
@@ -113,10 +134,56 @@ def getEvents(event_group_id):
 	except Exception as e:
 		return make_response(jsonify(error=e.__doc__), 500)
 
+@app.route("/bettingMarketGroups/<bmg_id>", methods=['GET'])
+def getBettingMarketGroup(bmg_id):
+	try:
+		return jsonify(bookie.getBettingMarketGroup(bmg_id))
+	except Exception as e:
+		return make_response(jsonify(error=e.__doc__), 500)
+
 @app.route("/events/<event_id>/bettingMarketGroups", methods=['GET'])
 def getBettingMarketGroups(event_id):
 	try:
 		return jsonify(bookie.getBettingMarketGroups(event_id))
+	except Exception as e:
+		return make_response(jsonify(error=e.__doc__), 500)
+
+@app.route("/bettingMarket", methods=['GET'])
+def getBettingMarketByQuery():
+	try:
+		sport = request.args.get("sport")
+		event_group = request.args.get("eventGroup")
+		event = request.args.get("event")
+		betting_market_group = request.args.get("bettingMarketGroup")
+		betting_market = request.args.get("bettingMarket")
+		for s in bookie.getSports():
+			if s['name'][0][1] == sport:
+				sport_id = s['id']
+				break
+		for eg in bookie.getEventGroups(sport_id):
+			if eg['name'][0][1] == event_group:
+				event_group_id = eg['id']
+				break
+		for e in bookie.getEvents(event_group_id):
+			if e['name'][0][1] == event:
+				event_id = e['id']
+				break
+		for bmg in bookie.getBettingMarketGroups(event_id):
+			if bmg['description'][0][1] == betting_market_group:
+				betting_market_group_id = bmg['id']
+				break
+		for bm in bookie.getBettingMarkets(betting_market_group_id):
+			if bm['description'][0][1] == betting_market:
+				bm_id = bm['id']
+				break
+		return jsonify(bookie.getBettingMarket(bm_id))
+	except Exception as e:
+		return make_response(jsonify(error=e.__doc__), 500)
+
+@app.route("/bettingMarkets/<betting_market_id>", methods=['GET'])
+def getBettingMarket(betting_market_id):
+	try:
+		return jsonify(bookie.getBettingMarket(betting_market_id))
 	except Exception as e:
 		return make_response(jsonify(error=e.__doc__), 500)
 
